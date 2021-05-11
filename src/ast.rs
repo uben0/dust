3ba4e57439642db4ce510f93b4bd66ff59b7e83e
uben <uben@protonmail.com>
2021-05-12 01:00:37 +0200

    manage functions

--- src/ast.rs
+++ src/ast.rs
@@ -1,12 +1,14 @@
 pub mod env;
 use env::*;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Expr {
     Value(Value),
     Ident(String),
     BinOp(BinOp, Box<Self>, Box<Self>),
     Seq(Vec<Stmt>, Option<Box<Self>>),
+    Apply(Box<Expr>, Box<Expr>),
+    Cond(Vec<(Expr, Expr)>),
 }
 impl Expr {
     pub fn eval(&self, stack: &mut EnvFrame<Value>) -> Value {
@@ -16,6 +18,12 @@ impl Expr {
                 || panic!("value {:?} not found in stack", i)
             ).clone(),
             Self::BinOp(o, l, r) => o.eval(l.eval(stack), r.eval(stack)),
+            Self::Cond(branches) => {
+                match branches.iter().find(|(c, _)| c.eval(stack).downcast_bool()) {
+                    Some((_, b)) => b.eval(stack),
+                    None => panic!("no valid branch found on cond control"),
+                }
+            }
             Self::Seq(s, v) => {
                 let mut stack = EnvFrame::new(Some(stack));
                 s.iter().for_each(|s| s.eval(&mut stack));
@@ -23,6 +31,30 @@ impl Expr {
                     v.eval(&mut stack)
                 } else {
                     Value::Int(0)
+                }
+            }
+            Self::Apply(f, p) => {
+                match f.eval(stack) {
+                    Value::Fn(rec, mut is, mut ps, e) => {
+                        if ps.len() < is.len() {
+                            ps.push(p.eval(stack));
+                            if ps.len() == is.len() {
+                                let mut env = EnvFrame::new(None);
+                                env.set(rec.clone(), Value::Fn(rec, is.clone(), Vec::new(), e.clone()));
+                                for (name, value) in is.drain(..).zip(ps.drain(..)) {
+                                    env.set(name, value);
+                                }
+                                e.eval(&mut env)
+                            }
+                            else {
+                                Value::Fn(rec, is, ps, e)
+                            }
+                        }
+                        else {
+                            panic!("unexpected apply on full function");
+                        }
+                    }
+                    v => panic!("cannot apply on: {}", v.type_name()),
                 }
             }
         }
@@ -55,9 +87,10 @@ impl Expr {
     // }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Stmt {
     Let(String, Expr),
+    Fn(String, Vec<String>, Expr),
     Mut(String, Expr),
     Eval(Expr),
     While(Expr, Expr),
@@ -69,6 +102,10 @@ impl Stmt {
         match self {
             Self::Let(i, e) => {
                 let val = e.eval(stack);
+                stack.set(i.to_owned(), val);
+            }
+            Self::Fn(i, p, e) => {
+                let val = Value::Fn(i.to_owned(), p.clone(), Vec::new(), (*e).clone().into());
                 stack.set(i.to_owned(), val);
             }
             Self::Mut(i, e) => {
@@ -148,12 +185,14 @@ impl BinOp {
 pub enum Value {
     Int(i32),
     Bool(bool),
+    Fn(String, Vec<String>, Vec<Value>, Box<Expr>),
 }
 impl Value {
     fn type_name(&self) -> &'static str {
         match self {
             Self::Int( ..) => "int",
             Self::Bool(..) => "bool",
+            Self::Fn(  ..) => "fn",
         }
     }
     fn downcast_bool(self) -> bool {
